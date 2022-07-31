a83647e19c602c5b96e8adfd4d1062b46a2db34c
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-08-01 01:58:55 +0500

    added accountController

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserControl.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult ListUsers()
+        {
+            var users = _userManager.Users;
+            return View(users);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
