7500e3330e1cd60267fc5f65d854ff3172c0e951
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-08-01 02:18:42 +0500

    output of all users to the page

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UserControl.Controllers
@@ -12,12 +13,14 @@ namespace UserControl.Controllers
             _userManager = userManager;
         }
 
+        [Authorize]
         [HttpGet]
         public IActionResult ListUsers()
         {
             var users = _userManager.Users;
             return View(users);
         }
+        [Authorize]
         public IActionResult Index()
         {
             return View();
