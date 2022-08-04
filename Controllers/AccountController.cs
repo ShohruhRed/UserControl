e5bcdff3f8c1b3853b82e93682738cf163d4796e
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-08-04 23:48:35 +0500

    implemented delete user

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using UserControl.Data;
 
 namespace UserControl.Controllers
@@ -8,10 +9,12 @@ namespace UserControl.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _dbContext;
 
-        public AccountController(UserManager<ApplicationUser> userManager)
+        public AccountController(UserManager<ApplicationUser> userManager, ApplicationDbContext dbContext)
         {
             _userManager = userManager;
+            _dbContext = dbContext;
         }
 
         [Authorize]
@@ -21,6 +24,37 @@ namespace UserControl.Controllers
             var users = _userManager.Users;
             return View(users);
         }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> DeleteSelected(string[] ids)
+        {
+            if (ids == null || ids.Length == 0)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {ids} cannot be found";
+                return View("NotFound");
+            }
+
+            else
+            {
+                //bind the task collection into list
+                List<string> TaskIds = new List<string>(ids);
+
+                for (var i = 0; i < TaskIds.Count(); i++)
+                {
+                    var user = await _userManager.FindByIdAsync(TaskIds[i]);
+                    //remove the record from the database
+                    _dbContext.Remove(user);
+                    //call save changes action otherwise the table will not be updated
+                    _dbContext.SaveChanges();
+                }
+
+                //redirect to index view once record is deleted
+                return RedirectToAction("ListUsers");
+            }
+            
+        }
+
         [Authorize]
         public IActionResult Index()
         {
